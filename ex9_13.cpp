679c2aad285c1d00dbb9beb08f841281d2d31756
wwShuang <galaxygu1636@gmail.com>
2020-10-05 22:49:57 +0800

    add ex9.13

---
+++ ex9_13.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+#include <dos.h>
+#include <conio.h>
+#include <string>
+#include <list>
+#include<vector>
+
+// @author @shbling @Yue Wang
+//
+// Exercise 9.13:
+// How would you initialize a vector<double> from a list<int>?
+// From a vector<int>?
+// Write code to check your answers.
+
+using std::list;
+using std::vector;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    list<int> ilst(5, 4);
+    vector<int> ivc(5, 5);
+
+    // from list<int> to vector<double>
+    vector<double> dvc(ilst.begin(), ilst.end());
+    for (auto i : ilst) cout << i << " ";
+    cout << endl;
+    for (auto d : dvc) cout << d << " ";
+    cout << endl;
+
+    // from vector<int> to vector<double>
+    vector<double> dvc2(ivc.begin(), ivc.end());
+    for (auto i : ivc) cout << i << " ";
+    cout << endl;
+    for (auto d : dvc2) cout << d << " ";
+
+    return 0;
+}
